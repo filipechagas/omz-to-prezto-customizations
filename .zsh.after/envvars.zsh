f4849751fb646b8dffdb35fcdc9841ad556e5717
Filipe Chagas <filipe@ochagas.com>
2013-01-25 13:43:32 -0200

    includes env vars

---
+++ .zsh.after/envvars.zsh
@@ -0,0 +1,9 @@
+# Set your git user info
+export GIT_AUTHOR_NAME='Filipe Chagas'
+export GIT_AUTHOR_EMAIL='filipe@ochagas.com'
+export GIT_COMMITTER_NAME='Filipe Chagas'
+export GIT_COMMITTER_EMAIL='filipe@ochagas.com'
+
+# Optionally, set your GitHub credentials
+export GITHUB_USER='filipechagas'
+#export GITHUB_TOKEN='your_github_token'
